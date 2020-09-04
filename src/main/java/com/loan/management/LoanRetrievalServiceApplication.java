0f5a149c376ea53254c068cc46137ca54854cf84
ctsjavauser283 <ctsjavauser283@ctsjavafs118.iiht.tech>
2020-09-04 10:56:20 +0530

    retrieval service integration with eureka

--- src/main/java/com/loan/management/LoanRetrievalServiceApplication.java
+++ src/main/java/com/loan/management/LoanRetrievalServiceApplication.java
@@ -2,8 +2,10 @@ package com.loan.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class LoanRetrievalServiceApplication {
 
 	public static void main(String[] args) {
